6e9a8038f4877c6432cd4b48af2b431ed4ee4f7f
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-08-01 23:51:35 +0200

    feat: Upgrade with React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import Main from './MainPage'
 import BookSearch from './SearchPage'
+import { Route } from 'react-router-dom'
+
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <BookSearch />
-        ) : ( <Main /> )}
 
-      </div>
+        <Route path="/search" render={() => (
+          <BookSearch />
+        )}/>
+
+        <Route path="/" exact render={() => (
+          <Main />
+        )}/>
+
+     </div>
     )
   }
 }
