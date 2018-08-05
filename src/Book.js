d38566682e7c9e5a2fe823b4751144a91e53e918
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-08-05 04:43:04 +0200

    refactor: Select Component

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Selector from './Selector'
+
 
 class Book extends Component {
 
@@ -16,7 +18,6 @@ class Book extends Component {
   }
 
 
-
   render() {
 
     let thumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''
@@ -32,16 +33,10 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${thumbnail}')` }}></div>
           <div className="book-shelf-changer">
-            <select
-              onChange={this.handleChange}
-              value={this.props.book.shelf}
-            >
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none" selected>None</option>
-            </select>
+              <Selector
+                book={this.props.book}
+                handleChange={this.handleChange}
+               />
           </div>
         </div>
         <div className="book-title">{this.props.book.title}</div>
